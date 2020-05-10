8945dd167dbba75268e6a7f9fb4c55a0526106f8
SuperArtisMickens <kdkartismickens@gmail.com>
2020-05-09 21:45:29 -0400

    First Push

--- Labs/lab18.cpp
+++ Labs/lab18.cpp
@@ -10,11 +10,22 @@ int T(int n)
         s = s * n;
         n -= 2;
     }
+    values[0] = s;
     return s;
 }
 
 int main()
 {    
     //Write all tasks below here
+    
+    int values [5];
+    values[2] = 12;
+    
+    values[4] = ((values[2] * 2) + 3);
+    
+    values[1] = values[0] - values[4];
+    
+    values[3] = ((values[0] + values[1] + values[2] + values[3] + values[4]) * 4);
+    
     return 0;
 }
--- Labs/lab19.cpp
+++ Labs/lab19.cpp
@@ -31,5 +31,20 @@ void TraceTable()
 int main()
 {    
     //Write call all tasks below here
+    
+    cout << "Trace Table\n";
+    cout << "-----------\n\n";
+    cout << "step  |A[]  |n    |fd   |i    |i<=n |A[i] == A[i+1] |fd = F |return |out\n";
+    cout << "------|-----|-----|-----|-----|-----|---------------|-------|-------|---\n";
+    cout << "01.   |a    |     |     |     |     |               |       |       |   \n";
+    cout << "02.   |     |10   |     |     |     |               |       |       |   \n";
+    cout << "03.   |     |     |F    |     |     |               |       |       |   \n";
+    cout << "04.   |     |     |     |0    |     |               |       |       |   \n";
+    cout << "05.   |     |     |     |     |T    |               |       |       |   \n";
+    cout << "06.   |     |     |     |     |     |F              |       |       |   \n";
+    cout << "07.   |     |     |T    |     |     |               |       |       |   \n";
+    cout << "08.   |     |     |     |1    |     |               |       |       |   \n";
+    cout << "09.   |     |     |     |     |     |               |       |TRUE   |   \n";
+ 
     return 0;
 }
